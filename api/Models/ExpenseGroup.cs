ba09b42b923d6e8de90f46f4ebe0625d4e53e82d
AlirieGray <alirie.gray@gmail.com>
2025-04-04 03:02:26 -0700

    add application db context

---
+++ api/Models/ExpenseGroup.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class ExpenseGroup
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; } = string.Empty;
+
+        public List<Expense> Expenses { get; set; } = new List<Expense>();
+    }
+}
