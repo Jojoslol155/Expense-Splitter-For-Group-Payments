d1cd2f24916c1d2f89682df7ef51b935f11afcfb
AlirieGray <alirie.gray@gmail.com>
2025-05-10 04:51:25 -0700

    empty expense group name handling

--- frontend/src/Pages/ExpenseGroup/ExpenseGroupPageHeader.tsx
+++ frontend/src/Pages/ExpenseGroup/ExpenseGroupPageHeader.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { Box, ButtonGroup, createTheme, Divider, TextField, ThemeProvider, Typography } from '@mui/material'
+import { Box, ButtonGroup, createTheme, Divider, TextField, ThemeProvider, Typography, Alert } from '@mui/material'
 import './ViewExpenseGroup.css'
 import MUIButton from '../../Components/MUIButton/MUIButton'
 import { Delete, Edit } from '@mui/icons-material'
@@ -21,20 +21,42 @@ const darkTheme = createTheme({
 const PageHeader = ({header, setOpenDeleteModal, groupID}: Props) => {
   const [ editingName, setEditingName ] = useState(false)
   const [ name, setName ] = useState(header)
-  console.log("name state")
-  console.log(name)
+  const [ showAlert, setShowAlert ] = useState(false)
+
   return (
+    <>
+    {showAlert && <div className='alertWrapper'>
+        <Alert severity='warning' onClose={() => {
+          setShowAlert(false)
+        }}>
+          Expense group name cannot be empty
+        </Alert>
+      </div>}
     <ThemeProvider theme={darkTheme}>
       <Box>
           <div className='pageHeaderWrapper'>
             {editingName ? (
               <TextField value={name} variant="standard" onBlur={() => {
                 setEditingName(false)
-
-                updateExpenseGroup(groupID, name)
-              }} onChange={(e) => {
-                console.log("change")
-                console.log(e.target.value)
+                if (name == "") {
+                  setShowAlert(true)
+                } else {
+                  setShowAlert(false)
+                  updateExpenseGroup(groupID, name)
+                }
+              }}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  setEditingName(false)
+                  if (name == "") {
+                    setShowAlert(true)
+                  } else {
+                    setShowAlert(false)
+                    updateExpenseGroup(groupID, name)
+                  }
+                }
+              }}
+              onChange={(e) => {
                 setName(e.target.value)
               }}>
 
@@ -61,6 +83,7 @@ const PageHeader = ({header, setOpenDeleteModal, groupID}: Props) => {
           <Divider sx={{ background: '#1D273B', padding: '3px', borderBottomWidth: '0'}} />
       </Box>
     </ThemeProvider>
+    </>
   )
 }
 
